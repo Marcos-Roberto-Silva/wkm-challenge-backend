eee33d195f0e1b9a30ea4ea9330c855e98127552
marcosR-silva <marcos.1998226@gmail.com>
2021-07-14 15:12:04 -0300

    Criado metodo delete cidades

--- src/services/CitiesService.js
+++ src/services/CitiesService.js
@@ -1,16 +1,15 @@
 const { Cities, States } = require("../models");
 const { cityAlreadyRegitered: message } = require("../utils/messageError");
-const { cepError } = require("../utils/messageError");
 const { Unauthorized: code } = require("../utils/httpStatus");
 
 const createCity = async ({ name, cep, uf }) => {
   const errorMessage = { message, code };
 
   const result = await Cities.findOne({ where: { cep } });
-  
+
   if (result && result.cep === cep) {
-     return errorMessage;
-  } 
+    return errorMessage;
+  }
 
   const city = await Cities.create({ name, cep, uf });
 
@@ -43,6 +42,11 @@ const getCityAndStateByCityName = async (name) => {
   return city;
 };
 
+const deleteCity = async ({ cep }) => {
+  const deletedCity = await Cities.destroy({ where: { cep } });
+  return deletedCity;
+};
+
 const updateCity = async ({ nParam, name, ufBody }) => {
   const state = await States.findOne({ where: { uf: ufParam } }).then(
     async (state) => {
@@ -60,4 +64,5 @@ module.exports = {
   getAllCities,
   citySearch,
   updateCity,
+  deleteCity,
 };
